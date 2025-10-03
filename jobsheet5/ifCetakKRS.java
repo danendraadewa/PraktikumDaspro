712b0bdcca7b786419171e0169e2af62ff31a90e
usernameGitHubKamu <emailGitHubKamu>
2025-10-03 21:14:52 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS.java
+++ jobsheet5/ifCetakKRS.java
@@ -12,7 +12,13 @@ public class ifCetakKRS {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
          } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+            System.out.println("Registrasi ditolak, Silahkan lunasi UKT terlebih dahulu");
+
+        String pesan = uktLunas
+            ? "Pembayaran UKT terverifikasi.Silahkan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak, Silahkan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
         }
     }
 }
